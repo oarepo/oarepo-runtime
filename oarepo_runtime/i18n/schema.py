7164e9b23df2dfa484af541246b9e107304a5e33
Alzpetka <lili3.pokorna@seznam.cz>
2023-03-14 14:12:57 +0100

    ui mult schema

--- oarepo_runtime/i18n/schema.py
+++ oarepo_runtime/i18n/schema.py
@@ -19,3 +19,15 @@ class MultilingualSchema(Schema):
 
 def MultilingualField(*args, **kwargs):
     return fields.List(fields.Nested(MultilingualSchema), *args, **kwargs)
+
+class MultilingualUISchema(Schema):
+    lang = fields.String(required=True)
+    value = fields.String(required=True)
+
+    @validates("lang")
+    def validate_lang(self, value):
+        if value != '_' and not langcodes.Language.get(value).is_valid():
+            raise ValidationError("Invalid language code")
+
+def MultilingualUIField(*args, **kwargs):
+    return fields.List(fields.Nested(MultilingualSchema), *args, **kwargs)
