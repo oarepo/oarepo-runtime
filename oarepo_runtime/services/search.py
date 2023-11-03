7248b7dc51be586b67732c1ed3d0429c0e35a02c
Juraj Trappl <juraj.trappl@proton.me>
2023-11-03 14:26:41 +0100

    add grouped facet param to search options

--- oarepo_runtime/services/search.py
+++ oarepo_runtime/services/search.py
@@ -8,7 +8,9 @@ from invenio_records_resources.proxies import current_service_registry
 from invenio_records_resources.services.records import (
     SearchOptions as InvenioSearchOptions,
 )
+from invenio_records_resources.services.records.params import QueryStrParam, PaginationParam, SortParam
 from invenio_records_resources.services.records.queryparser import SuggestQueryParser
+from oarepo_runtime.facets.params import GroupedFacetsParam
 
 from oarepo_runtime.records.systemfields.icu import ICUSuggestField
 
@@ -19,6 +21,8 @@ except ImportError:
 
 
 class SearchOptions(InvenioSearchOptions):
+    params_interpreters_cls = [QueryStrParam, PaginationParam, SortParam, GroupedFacetsParam]
+    
     sort_options = {
         "title": dict(
             title=_("By Title"),
