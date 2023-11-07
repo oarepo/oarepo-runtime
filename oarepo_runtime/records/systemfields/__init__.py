26e14b5232ee71001874c8e1184c5a9282f6e96c
Alzpetka <lili3.pokorna@seznam.cz>
2023-11-07 13:01:02 +0100

    imports fix

---
+++ oarepo_runtime/records/systemfields/__init__.py
@@ -0,0 +1,12 @@
+# from oarepo_runtime.records.systemfields.has_draftcheck import HasDraftCheckField
+# from oarepo_runtime.records.systemfields.icu import ICUField, ICUSortField, ICUSuggestField
+# from oarepo_runtime.records.systemfields.mapping import MappingSystemFieldMixin, SystemFieldDumperExt
+#
+# __all__ = (
+#     "ICUField",
+#     "ICUSuggestField",
+#     "ICUSortField",
+#     "MappingSystemFieldMixin",
+#     "SystemFieldDumperExt",
+#     "HasDraftCheckField",
+# )
