a9e62397e9bde5d21872b1b3e2059f3985475fa4
Alzpetka <lili3.pokorna@seznam.cz>
2023-11-08 08:00:34 +0100

    sort refactor

--- oarepo_runtime/records/systemfields/__init__.py
+++ oarepo_runtime/records/systemfields/__init__.py
@@ -1,12 +1,10 @@
-# from oarepo_runtime.records.systemfields.has_draftcheck import HasDraftCheckField
-# from oarepo_runtime.records.systemfields.icu import ICUField, ICUSortField, ICUSuggestField
-# from oarepo_runtime.records.systemfields.mapping import MappingSystemFieldMixin, SystemFieldDumperExt
-#
-# __all__ = (
-#     "ICUField",
-#     "ICUSuggestField",
-#     "ICUSortField",
-#     "MappingSystemFieldMixin",
-#     "SystemFieldDumperExt",
-#     "HasDraftCheckField",
-# )
+from .icu import ICUField, ICUSortField, ICUSuggestField
+from .mapping import MappingSystemFieldMixin, SystemFieldDumperExt
+
+__all__ = (
+    "ICUField",
+    "ICUSuggestField",
+    "ICUSortField",
+    "MappingSystemFieldMixin",
+    "SystemFieldDumperExt",
+)
