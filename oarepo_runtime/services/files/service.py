9c2a3bdf6c57473a8a6aab406233f608b200d359
Alzpetka <lili3.pokorna@seznam.cz>
2023-11-28 10:59:00 +0100

    systemfield

---
+++ oarepo_runtime/services/files/service.py
@@ -0,0 +1,58 @@
+import sys
+
+import click
+from invenio_records_resources.proxies import current_service_registry
+from invenio_records_resources.services.uow import unit_of_work
+from oarepo_runtime.cli import index
+from oarepo_runtime.cli.index import  model_records_generator
+from oarepo_runtime.datastreams.utils import get_file_service_for_record_service
+
+
+def reindex():
+    services = current_service_registry._services.keys()
+
+    for service_id in services:
+        service = current_service_registry.get(service_id)
+        record_class = getattr(service.config, "record_cls", None)
+
+        id_generators = []
+
+        if record_class and hasattr(service, "indexer"):
+            try:
+                id_generators.append(model_records_generator(record_class))
+            except Exception as e:
+                click.secho(
+                    f"Could not get record ids for {service_id}, exception {e}",
+                    file=sys.stderr,
+                )
+
+        draft_class = getattr(service.config, "draft_cls", None)
+
+        if draft_class and hasattr(service, "indexer"):
+            try:
+                id_generators.append(model_records_generator(draft_class))
+            except Exception as e:
+                click.secho(
+                    f"Could not get draft record ids for {service_id}, exception {e}",
+                    file=sys.stderr,
+                )
+        count = 0
+        for gen in id_generators:
+            for record in gen:
+                service.indexer.index(record)
+                count += 1
+        if count:
+            service.indexer.refresh()
+
+
+
+
+
+class FeaturedFileServiceMixin:
+    @unit_of_work()
+    def commit_file(self, identity, id_, file_key, uow=None):
+
+        # record = self._get_record(id_, identity, "commit_files", file_key=file_key)
+        reindex()
+
+
---
+++ tests/test_featured_file.py
@@ -0,0 +1,62 @@
+import tempfile
+from io import BytesIO
+from pathlib import Path
+
+import pytest
+from invenio_access.permissions import system_identity
+from records2.proxies import current_service
+from records2.records.api import Records2Record
+
+from oarepo_runtime.datastreams.fixtures import dump_fixtures, load_fixtures
+from oarepo_runtime.datastreams.types import StatsKeepingDataStreamCallback
+from oarepo_runtime.datastreams.utils import get_file_service_for_record_class
+from tests.test_fixtures import read_yaml
+
+# taken from https://github.com/inveniosoftware/invenio-records-resources/blob/master/tests/services/files/files_utils.py
+def add_file_to_record(file_service, recid, file_id, identity, data=None, featured=None):
+    """Add a file to the record."""
+    if featured:
+        file_service.init_files(identity, recid, data=[{"key": file_id, "featured": True}])
+
+    else:
+
+        file_service.init_files(identity, recid, data=[{"key": file_id}])
+    file_service.set_file_content(
+        identity,
+        recid,
+        file_id,
+        BytesIO(data or (b"test file content: " + file_id.encode("utf-8"))),
+    )
+    result = file_service.commit_file(identity, recid, file_id)
+    return result
+
+
+def test_create_rec_with_files(db, app, identity, search_clear, location):
+    rec = current_service.create(identity, {"metadata": {"title": "blah"}})
+    data = (
+        Path(__file__).parent / "file_data" / "files" / "001" / "data" / "test.png"
+    ).read_bytes()
+    add_file_to_record(
+        app.extensions["records2"].service_files,
+        rec.id,
+        "test.png",
+        identity,
+        data=data,
+
+    )
+    add_file_to_record(
+        app.extensions["records2"].service_files,
+        rec.id,
+        "another.png",
+        identity,
+        data=data,
+        featured=True
+    )
+    # current_service.scan(system_identity)
+    # current_service.read(system_identity, rec.id)
+
+    # Records2Record.index.refresh()
+    res = current_service.read(system_identity, rec.id)
+    assert 'featured' in res.data["metadata"]
+    print('record', res.data["metadata"])
+
