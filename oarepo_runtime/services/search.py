38d6130932328d04dd88f40c8156ca8a397e2b7a
Mirek Simek <miroslav.simek@gmail.com>
2023-12-12 17:34:55 +0100

    working on rdm 12 compatibility

--- oarepo_runtime/services/search.py
+++ oarepo_runtime/services/search.py
@@ -3,7 +3,8 @@ import inspect
 from typing import List
 
 # TODO: integrate this to invenio_records_resources.services.records and remove SearchOptions class
-from flask_babelex import lazy_gettext as _
+from oarepo_runtime.i18n import lazy_gettext as _
+
 from invenio_records_resources.proxies import current_service_registry
 from invenio_records_resources.services.records import (
     SearchOptions as InvenioSearchOptions,
