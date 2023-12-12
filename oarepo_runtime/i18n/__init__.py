0bce674a76f2a773cd1a1d0218fbdf977c55ddf0
Mirek Simek <miroslav.simek@gmail.com>
2023-12-12 18:29:35 +0100

    oarepo version check

--- oarepo_runtime/i18n/__init__.py
+++ oarepo_runtime/i18n/__init__.py
@@ -2,7 +2,7 @@ from oarepo import __version__ as oarepo_version
 
 # compatibility setting between invenio rdm 11 and invenio rdm 12
 # can be removed when invenio rdm 11 is no longer supported
-if oarepo_version.split('.')[0] == 11:
+if oarepo_version.split('.')[0] == "11":
     from flask_babelex import lazy_gettext
 else:
     from flask_babel import lazy_gettext
