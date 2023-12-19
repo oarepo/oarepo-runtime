df52387d55fd1e347b6ab97ee02dca5ef04c1534
Mirek Simek <miroslav.simek@gmail.com>
2023-12-19 19:55:29 +0100

    Fixing double creation bug in datastreams

--- oarepo_runtime/datastreams/writers/service.py
+++ oarepo_runtime/datastreams/writers/service.py
@@ -72,10 +72,15 @@ class ServiceWriter(BaseWriter):
 
         entry_id = self._get_stream_entry_id(stream_entry)
 
-        if entry_id and self._update:
-            repository_entry = self.try_update(entry_id, entry, **service_kwargs)
-            if repository_entry:
-                do_create = False
+        if entry_id:
+            if self._update:
+                repository_entry = self.try_update(entry_id, entry, **service_kwargs)
+                if repository_entry:
+                    do_create = False
+            else:
+                current = self._resolve(entry_id)
+                if current:
+                    do_create = False
 
         if do_create:
             repository_entry = self._service.create(
