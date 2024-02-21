e0857bd2a4f2587c9188971c0d59a85e95a2218a
Mirek Simek <miroslav.simek@gmail.com>
2024-02-21 11:19:03 +0100

    Changed @cached_property to @class_property as invenio-vocabularies, unlike other records, do not instantiate service config

--- oarepo_runtime/services/config/service.py
+++ oarepo_runtime/services/config/service.py
@@ -1,10 +1,11 @@
 import inspect
 import re
-from functools import cached_property
 from typing import List, Type
 
 from flask import current_app
 from invenio_records_permissions import BasePermissionPolicy
+
+from oarepo_runtime.utils.functools import class_property
 
 
 class PermissionsPresetsConfigMixin:
@@ -12,16 +13,19 @@ class PermissionsPresetsConfigMixin:
     PERMISSIONS_PRESETS_CONFIG_KEY = None
     # PERMISSIONS_PRESETS = []         # noqa (omitted here because of the order of mixins)
 
-    @cached_property
-    def permission_policy_cls(self):
+    @class_property
+    def permission_policy_cls(cls):
         """
         Returns a class that contains all permissions from the presets.
+
+        Needs to be a class property as invenio-vocabularies, unlike other invenio records,
+        do not instantiate the service configuration class, but use it as a class.
         """
-        presets = self._get_preset_classes()
+        presets = cls._get_preset_classes()
 
         permissions = {}
         for preset_class in presets:
-            for permission_name, permission_needs in self._get_permissions_from_preset(
+            for permission_name, permission_needs in cls._get_permissions_from_preset(
                 preset_class
             ):
                 target = permissions.setdefault(permission_name, [])
@@ -29,7 +33,7 @@ class PermissionsPresetsConfigMixin:
                     if need not in target:
                         target.append(need)
 
-        return type(f"{type(self).__name__}Permissions", tuple(presets), permissions)
+        return type(f"{cls.__name__}Permissions", tuple(presets), permissions)
 
     @staticmethod
     def _get_permissions_from_preset(preset_class):
@@ -40,7 +44,8 @@ class PermissionsPresetsConfigMixin:
                 continue
             yield permission_name, permission_needs
 
-    def _get_preset_classes(self) -> List[Type[BasePermissionPolicy]]:
+    @classmethod
+    def _get_preset_classes(cls) -> List[Type[BasePermissionPolicy]]:
         """
         Returns a list of permission presets classes to be used for this service.
 
@@ -50,25 +55,27 @@ class PermissionsPresetsConfigMixin:
         registered_preset_classes = current_app.config["OAREPO_PERMISSIONS_PRESETS"]
         preset_classes = [
             registered_preset_classes[x]
-            for x in self._get_permissions_presets()
+            for x in cls._get_permissions_presets()
         ]
-        if hasattr(self, "base_permission_policy_cls"):
-            preset_classes.insert(0, self.base_permission_policy_cls)
+        if hasattr(cls, "base_permission_policy_cls"):
+            preset_classes.insert(0, cls.base_permission_policy_cls)
         return preset_classes
 
-    def _get_permissions_presets(self):
+    @classmethod
+    def _get_permissions_presets(cls):
         """
         Returns a list of names of permissions presets to be used for this service.
 
         The list is read from the configuration, if it exists, otherwise it returns
         the default value from the class attribute PERMISSIONS_PRESETS.
         """
-        config_key = self._get_presents_config_key()
+        config_key = cls._get_presents_config_key()
         if config_key in current_app.config:
             return current_app.config[config_key]
-        return self.PERMISSIONS_PRESETS  # noqa (omitted here because of the order of mixins)
+        return cls.PERMISSIONS_PRESETS  # noqa (omitted here because of the order of mixins)
 
-    def _get_presents_config_key(self):
+    @classmethod
+    def _get_presents_config_key(cls):
         """
         Returns the name of the configuration key that contains the list of
         permissions presets to be used for this service.
@@ -77,13 +84,13 @@ class PermissionsPresetsConfigMixin:
         If it is not set, the key is generated from the name of the service
         configuration class (MyServiceConfig becomes MY_SERVICE_PERMISSIONS_PRESETS).
         """
-        if self.PERMISSIONS_PRESETS_CONFIG_KEY:
-            return self.PERMISSIONS_PRESETS_CONFIG_KEY
+        if cls.PERMISSIONS_PRESETS_CONFIG_KEY:
+            return cls.PERMISSIONS_PRESETS_CONFIG_KEY
 
         # use the base class of the service config file (e.g. MyServiceConfig),
         # remove the "Config" part and convert it to snake uppercase.
         # add "_PERMISSIONS_PRESETS" at the end.
-        name = type(self).__name__
+        name = cls.__name__
         if name.endswith("Config"):
             name = name[:-6]
         name = re.sub(r'(?<!^)(?=[A-Z])', '_', name).upper()
--- tests/test_permissions.py
+++ tests/test_permissions.py
@@ -9,32 +9,24 @@ def test_permissions_with_config_key(app, db, non_system_identity, search_clear,
     and checking that the identity is not allowed
     to create a record.
     """
+    # just to check that the cache invalidation below works
+    print(Records2ServiceConfig.permission_policy_cls)
     try:
         current_app.config['RECORDS2_SERVICE_PERMISSIONS_PRESETS'] = [
             "read_only"
         ]
-        # remove the previous value from the cache
-        try:
-            del Records2ServiceConfig.permission_policy_cls
-        except AttributeError:
-            pass
 
         permission_class = Records2ServiceConfig().permission_policy_cls
         permission_policy = permission_class("create")
         assert not permission_policy.allows(non_system_identity)
     finally:
+        setattr(Records2ServiceConfig, f"__cached__permission_policy_cls", None)
         del current_app.config['RECORDS2_SERVICE_PERMISSIONS_PRESETS']
 
 def test_permissions_without_config_key(app, db, non_system_identity, search_clear, location):
     """
     Default setting is everyone, so the identity should be able to create a record
     """
-
-    # remove the previous value from the cache
-    try:
-        del Records2ServiceConfig.permission_policy_cls
-    except AttributeError:
-        pass
 
     permission_class = Records2ServiceConfig().permission_policy_cls
     permission_policy = permission_class("create")
