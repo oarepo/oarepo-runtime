b309859b4319137ee26d3c879d8bc715b2a30454
Mirek Simek <miroslav.simek@gmail.com>
2024-03-27 10:08:48 +0100

    Refactored is_record, has_draft

--- oarepo_runtime/records/__init__.py
+++ oarepo_runtime/records/__init__.py
@@ -9,3 +9,19 @@ def select_record_for_update(record_cls: Type[Record], persistent_identifier):
     model_id = resolved_record.model.id
     obj = record_cls.model_cls.query.filter_by(id=model_id).with_for_update().one()
     return record_cls(obj.data, model=obj)
+
+
+def is_record(record, ctx):
+    """Shortcut for links to determine if record is a record."""
+    if not getattr(record, "is_draft", False):
+        return True
+    return not record.is_draft
+
+
+def has_draft(record, ctx):
+    """Shortcut for links to determine if record is a draft or a published one with a draft associated."""
+    if getattr(record, "is_draft", False):
+        return True
+    if getattr(record, 'has_draft', False):
+        return True
+    return False
