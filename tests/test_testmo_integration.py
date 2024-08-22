87709737ee65be626049dd3af2134e5ffb98f7b0
Miroslav Bauer <mirekys@users.noreply.github.com>
2024-08-22 11:55:23 +0200

    Test implicit step status in test_testmo_integration.py

--- tests/test_testmo_integration.py
+++ tests/test_testmo_integration.py
@@ -7,5 +7,5 @@ def test_testmo_integration(record_property):
     record_property("step[passed]", "Check if sky is blue")
     assert True
 
-    record_property("step[failed]", "Check if cows are purple")
+    record_property("step", "Check if cows are purple")
     assert False
