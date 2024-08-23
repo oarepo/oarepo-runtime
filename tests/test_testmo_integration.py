e33379bf1b9c877346845bed1cb8c8d95a40e8cc
brablcp <85112304+brablcp@users.noreply.github.com>
2024-08-23 11:28:00 +0200

    Update test_testmo_integration.py

--- tests/test_testmo_integration.py
+++ tests/test_testmo_integration.py
@@ -1,11 +1,11 @@
 def test_testmo_integration(record_property):
-    record_property("author", "mirekys")
-    record_property("text:description", "This test case evaluates integration features of Testmo")
-    record_property("url:github", "https://github.com/oarepo/oarepo-runtime/tests/test_testmo_integration.py")
+    #record_property("author", "mirekys")
+    #record_property("text:description", "This test case evaluates integration features of Testmo")
+    #record_property("url:github", "https://github.com/oarepo/oarepo-runtime/tests/test_testmo_integration.py")
   
     # Testmo also supports automation steps, including statuses & sub fields
-    record_property("step[passed]", "Check if sky is blue")
+    #record_property("step[passed]", "Check if sky is blue")
     assert True
 
-    record_property("step", "Check if cows are purple")
+    #record_property("step", "Check if cows are purple")
     assert False
