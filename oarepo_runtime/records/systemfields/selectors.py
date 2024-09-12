f46955590f3a29656ad50ff1c49cb95a2555a1fc
Ronald Krist <ronald.krist@cesnet.cz>
2024-09-12 10:30:21 +0200

    filtered and multi selectors

--- oarepo_runtime/records/systemfields/selectors.py
+++ oarepo_runtime/records/systemfields/selectors.py
@@ -1,4 +1,4 @@
-from typing import Any, List
+from typing import Any, List, Callable
 
 
 class Selector:
@@ -24,6 +24,59 @@ class FirstItemSelector(PathSelector):
             return [rec]
         return []
 
+class FilteredSelector(Selector):
+    """
+    Selector which filters output of another selector
+    Example:
+        FilteredSelector(PathSelector("metadata.creators", "metadata.contributors"),
+                                         filter=lambda x: x["nameType"] == "personal", projection="affiliations")
+
+    selects affiliations of creators with nameType personal from following data
+
+    data = {
+        "metadata": {
+            "creators": [
+                {"name": "hugo", "affiliations": ["uni1", "uni2"], "nameType": "personal"},
+                {"name": "uni3", "nameType": "organizational"},
+            ]
+        }
+    }
+    """
+    def __init__(self, selector: Selector, filter: Callable[[Any], bool], projection: Callable[[Any], Any]|str=None):
+
+        self.selector = selector
+        self.filter = filter
+        self.projection = projection
+
+    def select(self, record):
+        selected = self.selector.select(record)
+        selected = filter(self.filter, selected)
+        if self.projection:
+            ret = []
+            for select_element in selected:
+                if isinstance(self.projection, str):
+                    result = select_element[self.projection]
+                else:
+                    result = self.projection(select_element)
+                if isinstance(result, list):
+                    ret += result
+                else:
+                    ret.append(result)
+        else:
+            ret = list(selected)
+        return ret
+
+class MultiSelector(Selector):
+    """Selector concatenating outputs of multiple selectors"""
+    def __init__(self, *selectors: Selector):
+        self.selectors = selectors
+
+    def select(self, record):
+        ret = []
+        for selector in self.selectors:
+            ret += selector.select(record)
+        return ret
+
 
 def getter(data, path: List):
     if len(path) == 0:
@@ -37,3 +90,46 @@ def getter(data, path: List):
     elif isinstance(data, list):
         for item in data:
             yield from getter(item, path)
+"""
+def separate_element_condition(element):
+    if "?" in element:
+        return element.split("?")
+    return element, None
+
+
+def check(data, condition):
+    if condition:
+        key, value = condition.split("=")
+        return key in data and data[key] == value
+    else:
+        return True
+
+
+def getter(data, path: List, condition=None):
+    # allows selection from list of dicts based on one of the dict's values
+    if len(path) == 0:
+        if isinstance(data, list):
+            for item in data:
+                if check(item, condition):
+                    yield item
+        elif isinstance(data, dict):
+            if check(data, condition):
+                yield data
+        else:
+            yield data
+
+    elif isinstance(data, dict):
+        element, condition = separate_element_condition(path[0])
+        if element and element in data:
+            next = data[element]
+            if isinstance(next, list):
+                yield from getter(next, path[1:], condition)
+            elif isinstance(next, dict):
+                if check(data, condition):
+                    yield from getter(next, path[1:], None)
+
+    elif isinstance(data, list):
+        for item in data:
+            if check(item, condition):
+                yield from getter(item, path)
+"""
---
+++ tests/test_path_selector.py
@@ -0,0 +1,102 @@
+from oarepo_runtime.records.systemfields.selectors import getter, PathSelector, FilteredSelector, MultiSelector
+
+
+def test_filtered_selector():
+    data = {"metadata": {
+        "creators": [
+            {"name": "hugo", "affiliations": ["uni1", "uni2"], "nameType": "personal"},
+            {"name": "uni3", "nameType": "organizational"},
+        ]
+    }
+    }
+
+    selectors = [FilteredSelector(PathSelector("metadata.creators", "metadata.contributors"),
+                                         filter=lambda x: x["nameType"] == "personal", projection="affiliations"),
+                 FilteredSelector(PathSelector("metadata.creators", "metadata.contributors"),
+                                  filter=lambda x: x["nameType"] == "organizational")
+    ]
+
+    selector = MultiSelector(*selectors)
+    result = selector.select(data)
+    assert result == ['uni1', 'uni2', {'name': 'uni3', 'nameType': 'organizational'}]
+
+
+
+
+
+
+
+
+def test_getter():
+    data = {
+        "creators": [
+            {"name": "hugo", "affiliations": ["uni1", "uni2"], "nameType": "personal"},
+            {"name": "uni3", "nameType": "organizational"},
+        ]
+    }
+
+    result1 = list(getter(data, "creators?nameType=personal.affiliations".split(".")))
+    result2 = list(getter(data, "creators?nameType=organizational".split(".")))
+    result3 = list(getter(data, "creators".split(".")))
+    result4 = list(getter(data, "creators.affiliations".split(".")))
+
+    assert result1 == ["uni1", "uni2"]
+    assert result2 == [{"name": "uni3", "nameType": "organizational"}]
+    assert result3 == [
+        {"name": "hugo", "affiliations": ["uni1", "uni2"], "nameType": "personal"},
+        {"name": "uni3", "nameType": "organizational"},
+    ]
+    assert result4 == ["uni1", "uni2"]
+
+
+def test_getter_more_complex():
+    data = {
+        "creators": [
+            {
+                "name": {"u": {"university": {"org": "cvut", "city": "prag"}, "type": "technical"}, "v": "uni3"},
+                "nameType": "organizational",
+            },
+            {
+                "name": "hugo",
+                "affiliations": [
+                    {"institution": {"u": "uni1", "v": "uni2"}, "graduated": "yes"},
+                    {"institution": {"u": "uni3", "v": "uni3"}, "graduated": "no"},
+                ],
+                "nameType": "personal",
+            },
+        ]
+    }
+
+    result0 = list(
+        getter(
+            data,
+            "creators?nameType=personal.affiliations.institution?graduated=yes".split("."),
+        )
+    )
+
+    result = list(
+        getter(
+            data, "creators?nameType=organizational.name.u?v=uni3.university?type=technical".split(".")
+        )
+    )
+
+    result1 = list(
+        getter(data, "creators?nameType=personal.affiliations?graduated=yes".split("."))
+    )
+    result2 = list(
+        getter(
+            data,
+            "creators?nameType=personal.affiliations?graduated=yes.institution.u".split(
+                "."
+            ),
+        )
+    )
+    result3 = list(
+        getter(data, "creators?nameType=personal.affiliations.institution".split("."))
+    )
+    assert result == [{"org": "cvut", "city": "prag"}]
+    assert result0 == [{"u": "uni3", "v": "uni3"}]
+    assert result1 == [{"institution": {"u": "uni1", "v": "uni2"}, "graduated": "yes"}]
+    assert result2 == ["uni1"]
+    assert result3 == [{"u": "uni1", "v": "uni2"}, {"u": "uni3", "v": "uni3"}]
+
