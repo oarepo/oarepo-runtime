31d9c0d1c525bfcda2f5cbac6e11ca6a991b5623
Ronald Krist <ronald.krist@cesnet.cz>
2024-11-12 16:10:36 +0100

    keyword service template

---
+++ oarepo_runtime/services/keyword_entity/config.py
@@ -0,0 +1,12 @@
+from invenio_records_resources.services.base.config import ServiceConfig
+
+from ..results import ArrayRecordItem, ArrayRecordList
+from .schema import KeywordEntitySchema
+
+
+class KeywordEntityServiceConfig(ServiceConfig):
+    links_item = {}
+
+    result_item_cls = ArrayRecordItem
+    result_list_cls = ArrayRecordList
+    schema = KeywordEntitySchema
