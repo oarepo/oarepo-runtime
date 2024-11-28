3d80b18ce4471a429540229d26d7c5c1a5c8c926
Mirek Simek <miroslav.simek@gmail.com>
2024-11-28 10:25:48 +0100

    ISBN check

--- oarepo_runtime/services/schema/validation.py
+++ oarepo_runtime/services/schema/validation.py
@@ -11,9 +11,15 @@ from invenio_i18n import gettext as _
 
 def validate_identifier(value):
     try:
-        value["identifier"] = normalize_pid(
+        original_identifier = (value["identifier"] or '').strip()
+        normalized_identifier = normalize_pid(
             value["identifier"], value["scheme"].lower()
         )
+        if original_identifier and not normalized_identifier:
+            # the normalize_pid library has problems with isbn - does not raise an exception
+            # but returns an empty string
+            raise ValueError()
+        value["identifier"] = normalized_identifier
     except:
         raise ValidationError({
             "identifier": _("Invalid value %(identifier)s of identifier type %(type)s") % {"identifier": value['identifier'], "type": value['scheme']}
