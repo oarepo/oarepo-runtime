1f5b3b71ee03d93fc3ab25c8cc02464427742a72
Ronald Krist <ronald.krist@cesnet.cz>
2024-11-28 14:13:12 +0100

    deprecated calling link condtions from .records

--- oarepo_runtime/records/__init__.py
+++ oarepo_runtime/records/__init__.py
@@ -1,9 +1,10 @@
 from typing import Type
+from deprecated import deprecated
 
 from invenio_records_resources.records import Record
 from invenio_pidstore.errors import PIDDoesNotExistError, PIDUnregistered
-from ..datastreams.utils import get_record_service_for_record
-
+from ..datastreams.utils import get_record_service_for_record, get_file_service_for_record_class
+from invenio_records_resources.records.api import FileRecord
 
 def select_record_for_update(record_cls: Type[Record], persistent_identifier):
     """Select a record for update."""
@@ -13,42 +14,34 @@ def select_record_for_update(record_cls: Type[Record], persistent_identifier):
     return record_cls(obj.data, model=obj)
 
 
-def is_published_record(record, ctx):
-    """Shortcut for links to determine if record is a published record."""
-    return not getattr(record, "is_draft", False)
+@deprecated("Moved to oarepo_runtime.services.config.link_conditions")
+def is_published_record_function():
+    """Shortcut for links to determine if record is a published.
 
+    This function is deprecated. Use oarepo_runtime.services.config.is_published_record instead.
+    """
+    from oarepo_runtime.services.config.link_conditions import is_published_record
+    return is_published_record()
 
-def is_draft_record(record, ctx):
-    """Shortcut for links to determine if record is a draft record."""
-    return getattr(record, "is_draft", False)
+@deprecated("Moved to oarepo_runtime.services.config.link_conditions")
+def is_draft_record_function():
+    """Shortcut for links to determine if record is a draft record.
 
+    This function is deprecated. Use oarepo_runtime.services.config.is_draft_record instead.
+    """
+    from oarepo_runtime.services.config.link_conditions import is_draft_record
+    return is_draft_record()
 
-def has_draft(record, ctx):
-    """Shortcut for links to determine if record is either a draft or a published one with a draft associated."""
-    if getattr(record, "is_draft", False):
-        return True
-    if getattr(record, "has_draft", False):
-        return True
-    return False
+@deprecated("Moved to oarepo_runtime.services.config.link_conditions")
+def has_draft_function():
+    """Shortcut for links to determine if record is either a draft or a published one with a draft associated.
 
-class Condition:
-    def __init__(self, *conditions):
-        self.conditions = conditions
+    This function is deprecated. Use oarepo_runtime.services.config.has_draft instead.
+    """
+    from oarepo_runtime.services.config.link_conditions import has_draft
+    return has_draft()
 
-    def __call__(self, obj, ctx):
-        results = [cond(obj, ctx) for cond in self.conditions]
-        return all(results)
+is_published_record = is_published_record_function()
+is_draft = is_draft_record_function()
+has_draft = has_draft_function()
 
-def has_permission(action_name):
-    def _has_permission(record, ctx):
-        service = get_record_service_for_record(record)
-        return service.check_permission(action_name=action_name, record=record, **ctx) #will this always work without other arguments; ie will the requiered arguments always be in ctx?
-    return _has_permission
-
-def has_published_record(record, ctx):
-    service = get_record_service_for_record(record)
-    try:
-        service.record_cls.pid.resolve(record["id"])
-    except (PIDUnregistered, PIDDoesNotExistError):
-        return False
-    return True
