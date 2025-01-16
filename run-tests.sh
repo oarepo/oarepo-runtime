3b47204b736fb873d7a2420d7db09502e33b18c7
Alžběta Pokorná <68014925+Alzpeta@users.noreply.github.com>
2025-01-16 17:06:01 +0100

    test script (#244)
    * test script
    
    * upload artifact v4
    
    ---------
    
    Co-authored-by: Alzbeta Pokorna <alzbetapokorna@Alzbetas-MacBook-Pro.local>

--- run-tests.sh
+++ run-tests.sh
@@ -6,6 +6,9 @@ set -e
 
 OAREPO_VERSION="${OAREPO_VERSION:-12}"
 
+export PIP_EXTRA_INDEX_URL=https://gitlab.cesnet.cz/api/v4/projects/1408/packages/pypi/simple
+export UV_EXTRA_INDEX_URL=https://gitlab.cesnet.cz/api/v4/projects/1408/packages/pypi/simple
+
 BUILDER_VENV=.venv-builder
 if test -d $BUILDER_VENV ; then
 	rm -rf $BUILDER_VENV
@@ -39,21 +42,20 @@ $PYTHON -m venv $VENV
 pip install -U setuptools pip wheel nrp-devtools
 nrp-devtools proxy 120 &
 
-pip install "oarepo[tests, rdm]==${OAREPO_VERSION}.*" --index-url "http://127.0.0.1:4549/simple" --extra-index-url https://pypi.org/simple
-pip install -e ".[tests]" --index-url "http://127.0.0.1:4549/simple" --extra-index-url https://pypi.org/simple
-pip install -e records2 --index-url "http://127.0.0.1:4549/simple" --extra-index-url https://pypi.org/simple
+pip install "oarepo[tests, rdm]==${OAREPO_VERSION}.*"
+pip install -e ".[tests]"
+pip install -e records2
 pip install pytest-invenio
-# pip install -e records
-pip install -e thesis --index-url "http://127.0.0.1:4549/simple" --extra-index-url https://pypi.org/simple
+pip install -e thesis
 
 pip uninstall -y uritemplate
 pip install uritemplate
 
 invenio index destroy --force --yes-i-know || true
 
-## run OOM separately as it needs its own configuration of logging
-#pytest -m "not oom" tests
-#pytest -m "oom" tests
+# run OOM separately as it needs its own configuration of logging
+pytest -m "not oom" tests
+pytest -m "oom" tests
 
 
 test -d $VENV/var/instance || mkdir $VENV/var/instance
