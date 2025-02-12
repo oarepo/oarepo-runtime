e81ffd0158a2dcd63978f63f82163531faf61fa5
Alžběta Pokorná <68014925+Alzpeta@users.noreply.github.com>
2025-02-12 14:12:38 +0100

    rdm search options (#252)
    * rdm search options
    
    * rdm search options
    
    ---------
    
    Co-authored-by: Alzbeta Pokorna <alzbetapokorna@Alzbetas-MacBook-Pro.local>

--- oarepo_runtime/services/search.py
+++ oarepo_runtime/services/search.py
@@ -19,7 +19,7 @@ from oarepo_runtime.i18n import lazy_gettext as _
 from oarepo_runtime.records.systemfields.icu import ICUSuggestField
 
 from .facets.params import GroupedFacetsParam
-
+from invenio_rdm_records.services.config import RDMSearchOptions, RDMSearchDraftsOptions
 try:
     from invenio_i18n import get_locale
 except ImportError:
@@ -188,7 +188,14 @@ class ICUSortOptions:
             }
         return ret
 
-
 class I18nSearchOptions(SearchOptions):
+    extra_sort_options = {}
+    record_cls = None
+
+class I18nRDMSearchOptions(RDMSearchOptions):
+    extra_sort_options = {}
+    record_cls = None
+
+class I18nRDMDraftsSearchOptions(RDMSearchDraftsOptions):
     extra_sort_options = {}
     record_cls = None
