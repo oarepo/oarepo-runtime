693fd0174db65739168be6fad62d618c4f8ac1cc
Ronald Krist <ronald.krist@cesnet.cz>
2025-04-13 20:31:51 +0200

    added oai exportable response handler

--- oarepo_runtime/resources/responses.py
+++ oarepo_runtime/resources/responses.py
@@ -7,3 +7,14 @@ class ExportableResponseHandler(ResponseHandler):
         self.export_code = export_code
         self.name = name
         super().__init__(serializer, headers)
+
+
+class OAIExportableResponseHandler(ExportableResponseHandler):
+
+    def __init__(self, serializer, export_code, name, oai_code, oai_schema, oai_namespace, oai_serializer, headers=None):
+        """Constructor."""
+        self.oai_code = oai_code
+        self.oai_schema = oai_schema
+        self.oai_namespace = oai_namespace
+        self.oai_serializer = oai_serializer
+        super().__init__(serializer, export_code, name, headers)
