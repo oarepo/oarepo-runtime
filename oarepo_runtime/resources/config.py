f893be1ac714ecd9f7584934c7a879c0a7a4d544
Ronald Krist <ronald.krist@cesnet.cz>
2025-05-27 10:12:09 +0200

    redundant stuff deleted from BaseRecordResourceConfig

--- oarepo_runtime/resources/config.py
+++ oarepo_runtime/resources/config.py
@@ -1,11 +1,6 @@
-from flask_resources import (
-    JSONSerializer,
-    ResponseHandler,
-)
 from invenio_rdm_records.resources.config import (
     RDMRecordResourceConfig,
 )
-from invenio_records_resources.resources.records.headers import etag_headers
 
 class BaseRecordResourceConfig(RDMRecordResourceConfig):
     """Record resource configuration."""
@@ -15,13 +10,3 @@ class BaseRecordResourceConfig(RDMRecordResourceConfig):
 
     routes = RDMRecordResourceConfig.routes
     routes["all-prefix"] = "/all"
-
-    """
-    request_body_parsers = {
-        "application/json": RequestBodyParser(JSONDeserializer()),
-    }
-    """
-
-    """
-    response_handlers = record_serializers
-    """
