540d32685fc4916b64f1524584e1a784d9579117
Ronald Krist <ronald.krist@cesnet.cz>
2025-05-29 16:42:58 +0200

    PermissionRequiringLink(

--- oarepo_runtime/services/records/links.py
+++ oarepo_runtime/services/records/links.py
@@ -1,4 +1,5 @@
 from invenio_records_resources.services.base.links import Link
+from oarepo_runtime.services.config import has_permission
 
 def pagination_links_html(tpl: str)->dict[str, Link]:
     """Create pagination links (prev/selv/next) from the same template."""
@@ -19,3 +20,19 @@ def pagination_links_html(tpl: str)->dict[str, Link]:
             ),
         ),
     }
+
+class PermissionRequiringLink(Link):
+    def __init__(self, link: Link, permission: str):
+        """Constructor."""
+        self._permission = has_permission(permission)
+        self._link = link
+
+    def should_render(self, obj, ctx):
+        """Determine if the link should be rendered."""
+        if self._permission(obj, ctx):
+            return self._link.should_render(obj, ctx)
+        return False
+
+    def expand(self, obj, context):
+        """Expand the URI Template."""
+        return self._link.expand(obj, context)
