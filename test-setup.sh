b15a21fa7a6ac8eebc44d0d3b4a3dffdabd38088
Mirek Simek <miroslav.simek@gmail.com>
2025-08-10 22:41:13 +0200

    test setup script

--- test-setup.sh
+++ test-setup.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+#
+# This script sets up the testing environment by installing
+# the mock module from the tests directory.
+#
 source .venv/bin/activate
 
 echo "Installing mock-module to the current environment"
