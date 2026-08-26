5ecc15af992c34ed6ee76dd8d2fb57cf79bc9158
Mirek Simek <miroslav.simek@cesnet.cz>
2026-08-26 14:53:47 +0200

    extra test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,10 +8,18 @@
 #
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
+from invenio_access.permissions import system_identity
+from invenio_accounts.proxies import current_datastore
 from invenio_search.cli import destroy
 
+from oarepo_runtime import current_runtime
 from oarepo_runtime.cli.permissions import list_permissions
 from oarepo_runtime.cli.search import init
+
+if TYPE_CHECKING:
+    from invenio_accounts.models import Role
 
 
 def test_cli(app, search):
@@ -22,8 +30,23 @@ def test_cli(app, search):
     assert result.exit_code == 0
 
 
-def test_permission_cli(app, search, users):
+def test_permission_cli(app, db, search, users):
     """Test permission CLI commands."""
+    role: Role = current_datastore.create_role(name="test-role", description="Role used in the CLI test")
+    current_datastore.add_role_to_user(users[0].user, role)
+    current_datastore.commit()
+
     runner = app.test_cli_runner()
-    result = runner.invoke(list_permissions, ["vocabularies", users[0].user.email, "--detailed"])
+    result = runner.invoke(list_permissions, ["affiliations", users[0].user.email, "--detailed"])
+    assert result.exit_code == 0
+    assert "test-role" in result.output
+
+
+def test_permission_cli_with_existing_record(app, db, search_clear, users):
+    """Test permission CLI commands when an existing affiliation record id is passed."""
+    affiliations_service = current_runtime.models["affiliations"].service
+    affiliations_service.create(system_identity, {"id": "cern", "name": "CERN"})
+
+    runner = app.test_cli_runner()
+    result = runner.invoke(list_permissions, ["affiliations", users[0].user.email, "cern", "--detailed"])
     assert result.exit_code == 0
