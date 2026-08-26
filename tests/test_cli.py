05406f34a3d07bd60f996e65ebd6cabe350c386d
Mirek Simek <miroslav.simek@cesnet.cz>
2026-08-26 14:43:55 +0200

    fix: capturing exceptions for conditional generator

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,6 +10,7 @@ from __future__ import annotations
 
 from invenio_search.cli import destroy
 
+from oarepo_runtime.cli.permissions import list_permissions
 from oarepo_runtime.cli.search import init
 
 
@@ -18,4 +19,11 @@ def test_cli(app, search):
     runner = app.test_cli_runner()
     result = runner.invoke(destroy, "--yes-i-know")
     result = runner.invoke(init)
+    assert result.exit_code == 0
+
+
+def test_permission_cli(app, search, users):
+    """Test permission CLI commands."""
+    runner = app.test_cli_runner()
+    result = runner.invoke(list_permissions, ["vocabularies", users[0].user.email, "--detailed"])
     assert result.exit_code == 0
